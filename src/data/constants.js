23632ad3ade81c6a72f412565d0dc3e0653b56a3
Sujal Jain <63149580+Sujal942@users.noreply.github.com>
2024-10-08 01:43:53 +0530

    Portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,263 @@
+export const Bio = {
+  name: "Sujal Jain",
+  roles: ["Frontend Developer", "UI/UX Designer", "Programmer"],
+  description:
+    "I am a driven and versatile individual who enjoys taking on new challenges. I am passionate about learning and committed to delivering high-quality results. With an optimistic attitude and a growth mindset, I am prepared to make a significant contribution and achieve tremendous success.",
+  github: "https://github.com/Sujal942",
+  resume:
+    "https://drive.google.com/file/d/1XMqKJ_J84t7Wgp0epr0LJbfafc8fgwTf/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/sujal-jain-a7a34b1a6/",
+  twitter: "https://x.com/SujalJa13973286",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNhoXisDruJMDAq3Ltd-wuaMW2lGxck9wAKw&s",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Other Languages",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://avatars.githubusercontent.com/u/18133?s=200&v=4",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyKE11KsQHCwzSDmLTgRKEQp9RGunynOqZKA&s",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoUX5LMRa7atIsNfl0nP3DaUaV4URhV0PHfA&s",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 1,
+    img: "https://github.blog/wp-content/uploads/2022/10/hacktoberfestbanner.jpeg?resize=1200%2C630",
+    role: "Open Source Contributor ",
+    company: "Hacktoberfest 2024",
+    date: "Octomber 2024 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpOHZICt6EVYYLdri1FRDf8Ty0ou9cFFdEE7YxOEdTN8Un9-wmf5KT4cJounL-jUKr&usqp=CAU",
+    school: "Gyan Ganga Institute of Technology and Sciences, Jabalpur",
+    date: "2022 - 2026",
+    grade: "7.38 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Design at Gyan Ganga Institute of Technology and Sciences, Jabalpur.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Design",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV1Tdptjl_e56JB5UhK-08hxEM6u8YGIjHRQ&s",
+    school: "Maharishi Vidya Mandir",
+    date: "2021  - 2022",
+    desc: "I completed my class 12 high school education at Maharishi Vidya Mandir, Seoni Madhya Pradesh",
+    degree: "ISC(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV1Tdptjl_e56JB5UhK-08hxEM6u8YGIjHRQ&s",
+    school: "Maharishi Vidya Mandir",
+    date: "2019 - 2020",
+    desc: "I completed my class 10 education at Maharishi Vidya Mandir, Seoni Madhya Pradesh",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Ai Waqueel (frontEnd-Legal)",
+    description:
+      "This service offers legal guidance for understanding court cases and consultations",
+    tags: [
+      "React Js",
+      "Python",
+      "Node Js",
+      "Hugging Face (Data Set)",
+      "GIT",
+      "Github",
+      "API",
+      "Vercel",
+      "Streamlit",
+    ],
+    category: "web app",
+    github: "https://github.com/Sujal942/frontEnd-Legal",
+    webapp: "https://front-end-legal.vercel.app",
+  },
+  {
+    id: 2,
+    title: "GitHub Profile Readme Generator",
+    description:
+      "GitHub Profile Readme Generator allow users to easily customize and copy their GitHub profile Readme..",
+    tags: ["React Js", "Tailwind CSS", "API", "Vercel"],
+    category: "web app",
+    github: "https://github.com/Sujal942/react-github-readme-generator",
+    webapp: "https://react-github-readme-generator.vercel.app",
+  },
+  {
+    id: 3,
+    title: "Food WebApp",
+    description: "Online meal ordering via the food web app.",
+    tags: [
+      "React Js",
+      "Tailwind CSS",
+      "RazorPay Payment Gateway",
+      "Vercel",
+      "Toastify",
+      "Recoil",
+    ],
+    category: "web app",
+    github: "https://github.com/Sujal942/OIBSIP_PIZZA-SHOP",
+    webapp: "https://oibsip-pizza-shop.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "MetaCode IDE",
+    description:
+      "A platform that allows teachers and students to share codes and communicate online.",
+    tags: [
+      "React Js",
+      "Tailwind CSS",
+      "SocketIO",
+      "Express,",
+      "Vercel",
+      "API,",
+      "GIT",
+      "GitHub",
+    ],
+    category: "web app",
+    github: "https://github.com/Sujal942/MetaCode",
+    webapp: "https://meta-code-cyan.vercel.app",
+  },
+  {
+    id: 5,
+    title: "QrCode-Generator",
+    description: "A platform that allows you to generate Qr Code Easily",
+    tags: ["React Js", "Tailwind CSS", "Vercel", "GitHub"],
+    category: "web app",
+    github: "https://github.com/Sujal942/QrCode-Generator-",
+    webapp: "https://qr-code-generator-liart-one.vercel.app",
+  },
+  {
+    id: 6,
+    title: "Pokemon App",
+    description: "A platform that displays the different Pokemon",
+    tags: ["React Js", "Tailwind CSS", "Vercel", "API"],
+    category: "web app",
+    github: "https://github.com/Sujal942/Pokemon-App",
+    webapp: "https://pokemon-app-six-ashy.vercel.app",
+  },
+  {
+    id: 7,
+    title: "Weather App",
+
+    description:
+      "A platform that displays the various weather conditions of different locations.",
+    tags: ["React Js", "Tailwind CSS", "Vercel", "API"],
+    category: "web app",
+    github: "https://github.com/Sujal942/Weather-App",
+    webapp: "https://weather-app-seven-iota-65.vercel.app",
+  },
+  {
+    id: 8,
+    title: "Currency Convetor App",
+    description: "A platform that convert currency from one to another.",
+
+    tags: ["React Js", "Tailwind CSS", "Vercel", "API"],
+    category: "web app",
+    github: "https://github.com/Sujal942/currency-converter-app",
+    webapp: "",
+  },
+];
